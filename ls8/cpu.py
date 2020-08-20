6389540653af22fd79372ab49723f22092f51bf7
Handell Desulme <tue73811@temple.edu>
2020-08-20 17:35:21 -0400

    implemented branch table to make code more readable

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,18 @@ class CPU:
         self.sp = 7
         self.ram = [0] * 256 # 256 8-bit addresses
         self.reg = [0] * 8 # 8 general-purpose registers
+        self.running = False
+        self.BT = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b10100010: self.MUL,
+            0b10100000: self.ADD,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET,
+            0b00000001: self.HALT
+        }
 
     def load(self, filename):
         """Load a program into memory."""
@@ -65,100 +77,73 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
+    def LDI(self, operand_a, operand_b, pc):
+        self.reg[operand_a] = operand_b
+        return pc + 3
+
+    def PRN(self, operand_a, operand_b, pc):
+        print(self.reg[operand_a])
+        return pc + 2
+
+    def MUL(self, operand_a, operand_b, pc):
+        self.reg[operand_a] *= self.reg[operand_b]
+        return pc + 3
+
+    def ADD(self, operand_a, operand_b, pc):
+        self.reg[operand_a] += self.reg[operand_b]
+        return pc + 3
+
+    def PUSH(self, operand_a, operand_b, pc):
+        self.reg[self.sp] -= 1
+        self.ram_write(self.reg[self.sp], self.reg[operand_a])
+        return pc + 2
+
+    def POP(self, operand_a, operand_b, pc):
+        self.reg[operand_a] = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+        return pc + 2
+    
+    def CALL(self, operand_a, operand_b, pc):
+        self.reg[self.sp] -= 1
+        self.ram_write(self.reg[self.sp], self.pc+2)
+        self.pc = self.reg[operand_a]
+        return self.pc
+    
+    def RET(self, operand_a, operand_b, pc):
+        self.pc = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+        return self.pc
+
+    def HALT(self, operand_a, operand_b, pc):
+        self.running = False
+        return -1
+
     def run(self): #FETCH, DECODE, EXECUTE
         """Run the CPU."""
-        running = True
+        self.running = True
         self.reg[self.sp] = 0xF4 #SP = 244
-        while running:
+        while self.running:
             IR = self.ram_read(self.pc) #Instruction Register
             OP_A = self.ram_read(self.pc+1) #Operand 1
             OP_B = self.ram_read(self.pc+2) #Operand 2
-            if IR == 0b10000010: #LDI
-                self.reg[OP_A] = OP_B
-                op_size = 2
-            elif IR == 0b01000111: #PRN
-                print(self.reg[OP_A])
-                op_size = 1
-            elif IR == 0b10100010: #MUL
-                self.reg[OP_A] *= self.reg[OP_B]
-                op_size = 2
-            elif IR == 0b01000101: #PUSH
-                self.reg[self.sp] -= 1
-                self.ram_write(self.reg[self.sp], self.reg[OP_A])
-                op_size = 1
-            elif IR == 0b01000110: #POP
-                self.reg[OP_A] = self.ram_read(self.reg[self.sp])
-                self.reg[self.sp] += 1
-                op_size = 1
-            elif IR == 0b01010000: #CALL
-                # push the return address on to the stack
-                self.reg[self.sp] -= 1
-                self.ram_write(self.reg[self.sp], self.pc+2)
-                # set the pc to the subroutines address
-                self.pc = self.reg[OP_A]
-                op_size = -1
-            elif IR == 0b00010001: #RET
-                # POP return address from stack to store in pc
-                self.pc = self.ram_read(self.reg[self.sp])
-                self.reg[self.sp] += 1
-                op_size = -1
-            elif IR == 0b10100000: #ADD
-                self.reg[OP_A] += self.reg[OP_B]
-                op_size = 2
-            elif IR == 0b00000001: #HLT
-                running = self.HLT()
-
-            self.pc += op_size + 1
-
-    def HLT(self):
-        return False
-
-    def LDI(self, operand_a, operand_b):
-        self.reg[operand_a] = operand_b
-        return 3
-
-    def PRN(self, operand_a, operand_b):
-        print(self.reg[operand_a])
-        return 2
-
-    def MUL(self, operand_a, operand_b):
-        self.reg[operand_a] *= self.reg[operand_b]
-        return 3
-
-    def PUSH(self, operand_a, operand_b):
-        self.reg[self.sp] -= 1
-        self.ram_write(self.reg[self.sp], self.reg[operand_a])
-        return 2
-
-    def POP(self, operand_a, operand_b):
-        self.reg[operand_a] = self.ram_read(self.reg[self.sp])
-        self.reg[self.sp] += 1
-        return 2
-
-    def HALT(self, operand_a, operand_b):
-        return False
-
-        BT = {
-        0b10000010: LDI,
-        0b01000111: PRN,
-        0b10100010: MUL,
-        0b01000101: PUSH,
-        0b01000110: POP,
-        0b00000001: HALT
-    }
+            if IR in self.BT:
+                self.pc = self.BT[IR](OP_A, OP_B, self.pc)
+            else:
+                print("Invalid")
+                
 
 
-cpu1 = CPU()
-cpu2 = CPU()
-cpu3 = CPU()
+# cpu1 = CPU()
+# cpu2 = CPU()
+# cpu3 = CPU()
 cpu4 = CPU()
 
-cpu1.load("./ls8/examples/print8.ls8")
-cpu1.run()
-cpu2.load("./ls8/examples/mult.ls8")
-cpu2.run()
-cpu3.load("./ls8/examples/stack.ls8")
-cpu3.run()
+# cpu1.load("./ls8/examples/print8.ls8")
+# cpu1.run()
+# cpu2.load("./ls8/examples/mult.ls8")
+# cpu2.run()
+# cpu3.load("./ls8/examples/stack.ls8")
+# cpu3.run()
 cpu4.load("./ls8/examples/call.ls8")
 cpu4.run()
 
